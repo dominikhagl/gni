eb44dce18a7fb9282995a6e90836dc5920924dfc
dominikhagl <github@dominik-hagl.de>
2013-12-06 17:45:47 +0100

    Fixed deuflhard

--- deuflhard.m
+++ deuflhard.m
@@ -35,15 +35,15 @@ function [ q, dq ] = deuflhard(omega, g, q0, dq0, h, N, progress)
     ho = h*omega;
     
     %initialize second startvalue (two-step formulation)
-    q1(:) = cos(ho)*q0(:) + sinc(ho)*h*dq0(:) + h^2*sinc(ho)*g(q0(:));
-    dq1(:) = -ho*sin(ho)*q0(:) + cos(ho)*h*dq0(:) + h^2*(g(q1(:)) + cos(ho)*g(q0(:)));
+    q1 = cos(ho)*q0(:) + sinc(ho)*h*dq0(:) + h^2*sinc(ho)*g(q0(:));
+    dq1 = -ho*sin(ho)*q0(:) + cos(ho)*h*dq0(:) + h^2*(g(q1(:)) + cos(ho)*g(q0(:)));
     
     q = [q0 q1 zeros(dim, N)];
     dq = [dq0 dq1 zeros(dim, N)];
 
-    for n = 1:N;
+    for n = 3:N;
         q(:, n) = h^2*sinc(ho)*g(q(:, n-1)) + 2*cos(ho)*q(:, n-1) - q(:, n-2);
-        dq(:, n) = h*(g(:, n)-g(:, n-2))/2 + 2*cos(ho)*dq(:, n-1) - dq(:, n-2);
+        dq(:, n) = h*(g(q(n))-g(q(n-2)))/2 + 2*cos(ho)*dq(:, n-1) - dq(:, n-2);
 
         if progress && mod(n, percent) == 0
             fprintf('\b\b\b\b\b%4d%%', n/percent);
